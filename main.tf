9a4e30b3ae910325a2c1d9ae871c874cec518d0a
Eddie Webb <ollitech@gmail.com>
2023-09-29 11:54:35 -0400

    cleanup

--- main.tf
+++ main.tf
@@ -7,17 +7,16 @@ resource "vault_jwt_auth_backend" "awesomeci_oidc" {
 
 resource "vault_mount" "kvv2" {
   path = "secret"
-  type = "kv-v2"
+  type = "kv"
   options = {
     version = "2"
-    type    = "kv-v2"
   }
   description = "This is an example KV Version 2 secret engine mount"
 }
 
 resource "vault_kv_secret_backend_v2" "example" {
-  mount                = vault_mount.kvv2.path
-  cas_required         = false
+  mount        = vault_mount.kvv2.path
+  cas_required = false
 }
 
 resource "vault_policy" "nexus_access" {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
+
+# So thst other modules can share secrets to vault
 output "vault_mount_path" {
   value = vault_mount.kvv2.path
 }
