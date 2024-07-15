0f4ab9cb128a65af084c62b9644969cc00e10041
james-crowley <jim.crowley@circleci.com>
2024-07-15 16:11:05 -0400

    ecap roles [semver:patch]

--- roles_policies_ecap.tf
+++ roles_policies_ecap.tf
@@ -6,7 +6,7 @@
 resource "vault_jwt_auth_backend_role" "ecap_dev" {
   backend        = vault_jwt_auth_backend.ecap_oidc.path
   role_name      = "ecap-dev-deploy"
-  token_policies = ["nexus-deploy-access", "ecap-dev-deploy"]
+  token_policies = ["nexus-deploy-access", "boa-dev-deploy"]
 
   bound_claims = {
     "oidc.circleci.com/context-ids" = "d05c8c0b-5048-4dc6-a998-8c3d6bc672d5"
@@ -20,7 +20,7 @@ resource "vault_jwt_auth_backend_role" "ecap_dev" {
 resource "vault_jwt_auth_backend_role" "ecap_prod" {
   backend        = vault_jwt_auth_backend.ecap_oidc.path
   role_name      = "ecap-prod-deploy"
-  token_policies = ["nexus-deploy-access", "ecap-prod-deploy"]
+  token_policies = ["nexus-deploy-access", "boa-prod-deploy"]
 
   bound_claims = {
     "oidc.circleci.com/context-ids" = "136e5d23-920c-4304-9b96-c6792931d9b1"
@@ -33,39 +33,3 @@ resource "vault_jwt_auth_backend_role" "ecap_prod" {
 
 
 ## Policies (used by roles)
-
-
-
-
-resource "vault_policy" "ecap-dev" {
-  name = "ecap-dev-deploy"
-
-  policy = <<EOF
-path "secret/*" {
-  capabilities = ["list"]
-}
-path "secret/data/cluster/ecap-dev" {
-  capabilities = ["list","read"]
-}
-path "secret/metadata/cluster/ecap-dev" {
-  capabilities = ["list","read"]
-}
-EOF
-}
-
-
-resource "vault_policy" "ecap_prod" {
-  name = "ecap-prod-deploy"
-
-  policy = <<EOF
-path "secret/*" {
-  capabilities = ["list"]
-}
-path "secret/data/cluster/ecap" {
-  capabilities = ["list","read"]
-}
-path "secret/metadata/cluster/ecap" {
-  capabilities = ["list","read"]
-}
-EOF
-}
