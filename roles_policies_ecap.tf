fe37d5b8e0e4d06cd46d226c6dd2f38967c73fc1
james-crowley <jim.crowley@circleci.com>
2024-07-15 16:14:38 -0400

    ecap roles [semver:patch]

--- roles_policies_ecap.tf
+++ roles_policies_ecap.tf
@@ -6,7 +6,7 @@
 resource "vault_jwt_auth_backend_role" "ecap_dev" {
   backend        = vault_jwt_auth_backend.ecap_oidc.path
   role_name      = "ecap-dev-deploy"
-  token_policies = ["nexus-deploy-access", "boa-dev-deploy"]
+  token_policies = ["nexus-deploy-access", "cba-dev-deploy"]
 
   bound_claims = {
     "oidc.circleci.com/context-ids" = "d05c8c0b-5048-4dc6-a998-8c3d6bc672d5"
@@ -20,7 +20,7 @@ resource "vault_jwt_auth_backend_role" "ecap_dev" {
 resource "vault_jwt_auth_backend_role" "ecap_prod" {
   backend        = vault_jwt_auth_backend.ecap_oidc.path
   role_name      = "ecap-prod-deploy"
-  token_policies = ["nexus-deploy-access", "boa-prod-deploy"]
+  token_policies = ["nexus-deploy-access", "cba-prod-deploy"]
 
   bound_claims = {
     "oidc.circleci.com/context-ids" = "136e5d23-920c-4304-9b96-c6792931d9b1"
