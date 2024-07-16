61d0173f8f149918a65918e3d8ff5f3a97b31389
james-crowley <jim.crowley@circleci.com>
2024-07-16 18:19:32 -0400

    updating ecap for c1 demo

--- roles_policies_ecap.tf
+++ roles_policies_ecap.tf
@@ -9,8 +9,8 @@ resource "vault_jwt_auth_backend_role" "ecap_dev" {
   token_policies = ["nexus-deploy-access", "cba-dev-deploy"]
 
   bound_claims = {
-    "oidc.circleci.com/context-ids" = "d05c8c0b-5048-4dc6-a998-8c3d6bc672d5"
-    "oidc.circleci.com/project-id"  = "a9d88e1c-c012-4304-95da-87259abfccdc"
+    "oidc.circleci.com/context-ids" = "f5643782-9c02-48a8-b59b-bc7eb6e45f8d"
+    "oidc.circleci.com/project-id"  = "5f5f503c-dd77-4ee9-b436-7e032538e2be"
   }
   user_claim              = "sub"
   role_type               = "jwt"
@@ -23,8 +23,8 @@ resource "vault_jwt_auth_backend_role" "ecap_prod" {
   token_policies = ["nexus-deploy-access", "cba-prod-deploy"]
 
   bound_claims = {
-    "oidc.circleci.com/context-ids" = "136e5d23-920c-4304-9b96-c6792931d9b1"
-    "oidc.circleci.com/project-id"  = "a9d88e1c-c012-4304-95da-87259abfccdc"
+    "oidc.circleci.com/context-ids" = "8c0765c6-b93c-4e15-aa29-b8dc24ea8c1f"
+    "oidc.circleci.com/project-id"  = "5f5f503c-dd77-4ee9-b436-7e032538e2be"
   }
   user_claim              = "sub"
   role_type               = "jwt"
